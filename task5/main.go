401918e57062b25a22debf3c118ce8a76b01ec39
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2026-03-06 13:38:11 +0300

    Update main.go

--- task5/main.go
+++ task5/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	// Use your local MongoDB URI
-	data.ConnectDB("mongodb+srv://welelabekeleug_db_user:q9sC8pS2xZJYfWZJ@cluster0.n6xzoew.mongodb.net/task5?retryWrites=true&w=majority")
+	data.ConnectDB("mongodb+srv://welelabekeleug_db_user:password@cluster0.n6xzoew.mongodb.net/task5?retryWrites=true&w=majority")
 
 	r := router.SetupRouter()
 	r.Run(":8080")
